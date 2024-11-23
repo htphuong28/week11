60465468d175d12abdb353e8f3ed00dea4a88253
htphuong28 <thieuphuong5knt@gmail.com>
2024-11-24 00:28:06 +0200

    add item

--- GroceriesGame/groceries/Item.scala
+++ GroceriesGame/groceries/Item.scala
@@ -2,11 +2,13 @@ package groceries
 
 /** The class 'Item' represents items that the character will collect and bring home.
  *
- * @param name
+ * @param name...........item's name
+ * @param description....describe item
+ * @param isConsumable...determines whether item is consumable or not
  *
  */
 
-class Item {val name: String, val description: String, val isConsumable: Boolean):
+class Item (val name: String, val description: String, val isConsumable: Boolean):
   def isGoodCondition = isConsumable
   override def toString = this.name
 
