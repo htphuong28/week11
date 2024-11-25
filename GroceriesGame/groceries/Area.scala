5573f582af4c77bcea5c33480c757b3ebe662bb6
hoanghuudao <huudao2536@gmail.com>
2024-11-25 23:26:37 +0100

    action and area

---
+++ GroceriesGame/groceries/Area.scala
@@ -0,0 +1,31 @@
+import scala.collection.mutable.Map
+
+class Area(var name: String, var description: String):
+
+  private val neighbors = Map[String, Area]()
+  //private val items = Map[String, Item]()
+
+  /** def addItem(item: Item) = this.items += item.name -> item
+  def contains(itemName: String): Boolean = items.contains(itemName)
+  def removeItem(itemName: String) = items.remove(itemName) */
+
+  def neighbor(direction: String) = this.neighbors.get(direction)
+
+  def setNeighbor(direction: String, neighbor: Area) =
+    this.neighbors += direction -> neighbor
+
+  def setNeighbors(exits: Vector[(String, Area)]) =
+    this.neighbors ++= exits
+
+  def fullDescription: String =
+    //val pickUpItemList = if items.nonEmpty then "\nYou see here: " + this.items.keys.mkString(" ") else ""
+    //val forSaleItemList = if items.nonEmpty then "\nThe vendors here sell: " + this.items.keys.mkString(" ") else ""
+    //val npcList
+    val exitList = "\n\nExits available: " + this.neighbors.keys.mkString(" ")
+    this.description + /* pickUpItemList + forSaleItemList + npcList + */ exitList
+
+  /** Returns a single-line description of the area for debugging purposes. */
+  override def toString =
+    this.name + ": " + this.description.replaceAll("\n", " ").take(150)
+
+end Area
