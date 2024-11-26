c5d0770d64539c554d79fe3a86f370621e66ecf4
htphuong28 <thieuphuong5knt@gmail.com>
2024-11-26 18:20:26 +0200

    item

--- GroceriesGame/groceries/Item.scala
+++ GroceriesGame/groceries/Item.scala
@@ -4,12 +4,19 @@ package groceries
  *
  * @param name...........item's name
  * @param description....describe item
- * @param isConsumable...determines whether item is consumable or not
+ * @param isConsumable...determines whether an item is consumable or not
+ * @param isOnSale       determines whether an item is on sale or not
  *
  */
 
-class Item (val name: String, val description: String, val isConsumable: Boolean):
+class Item (val name: String, val description: String, val isConsumable: Boolean, private var isOnSale: Boolean):
+
   def isGoodCondition = isConsumable
+  
+  def isBuyable = isOnSale
+
+
+
   override def toString = this.name
 
 
