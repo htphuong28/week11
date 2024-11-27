40a1cbd15873fb3e08c676ab42a9e28c25b10d1d
htphuong28 <thieuphuong5knt@gmail.com>
2024-11-27 02:23:34 +0200

    minor change

--- GroceriesGame/groceries/NPC.scala
+++ GroceriesGame/groceries/NPC.scala
@@ -1,5 +1,30 @@
 package groceries
+<<<<<<< Updated upstream
 
 class NPC { 
   
 }
+
+import util.Random
+
+trait NPC (name: String, description: String):
+  def greeting: String = "Good morning. I'm here to ask you a question. Do you want to answer it?"
+
+  def userInteraction: Nothing
+
+  override def toString = this.description
+
+
+class HelperNPC(name: String, description: String) extends NPC(name, description):
+  val num1 = Random(3)
+  val num2 = Random(5)
+  def userInteraction = 
+    "Here's your question: " + num1 + "+" + num2 + "= ?"
+    
+  def checkAns(input: Int) =
+    if input == num1 + num2 then 
+      "Congrats"
+         
+    else 
+      "Nuh uh"
+>>>>>>> Stashed changes
